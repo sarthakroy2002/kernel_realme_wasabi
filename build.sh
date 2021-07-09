sarthakroy2002/kernel_realme_RMX2020

script: Add build script
Change-Id: Id320bce27bd5da41218b4a95d2864c0ed33bea45
Signed-off-by: sarthakroy2002 <sarthak.roy@s.amity.edu>

build.sh
@@ -1,0 +1,42 @@
+#!/bin/bash
+
+function compile() 
+{
+
+source ~/.bashrc && source ~/.profile
+export LC_ALL=C && export USE_CCACHE=1
+ccache -M 100G
+export ARCH=arm64
+export KBUILD_BUILD_HOST=neolit
+export KBUILD_BUILD_USER="sarthakroy2002"
+git clone --depth=1 https://github.com/sarthakroy2002/clang-r353983c1 clang
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
+
+[ -d "out" ] && rm -rf out || mkdir -p out
+
+make O=out ARCH=arm64 rmx2020_defconfig
+
+PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC="clang" \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-android-" \
+                      CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-androideabi-" \
+                      CONFIG_NO_ERROR_ON_MISMATCH=y
+}
+
+function zupload()
+{
+git clone --depth=1 https://github.com/sarthakroy2002/AnyKernel3.git AnyKernel
+cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+cd AnyKernel
+zip -r9 Test-OSS-KERNEL-RMX2020-NEOLIT.zip *
+#curl --upload-file Test-OSS-KERNEL-RMX2020-NEOLIT.zip https://transfer.sh/
+curl -sL https://git.io/file-transfer | sh
+./transfer wet Test-OSS-KERNEL-RMX2020-NEOLIT.zip
+}
+
+compile
+zupload

sarthakroy2002/kernel_realme_wasabi

build.sh
@@ -1,0 +1,43 @@
+#!/bin/bash
+
+function compile() 
+{
+
+source ~/.bashrc && source ~/.profile
+export LC_ALL=C && export USE_CCACHE=1
+ccache -M 100G
+export ARCH=arm64
+export KBUILD_BUILD_HOST=neolit
+export KBUILD_BUILD_USER="sarthakroy2002"
+git clone --depth=1 https://github.com/sarthakroy2002/clang-r353983c1 clang
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
+
+[ -d "out" ] && rm -rf out || mkdir -p out
+
+make O=out ARCH=arm64 RMX2001_defconfig
+
+PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC="clang" \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-android-" \
+                      CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-androideabi-" \
+                      CONFIG_NO_ERROR_ON_MISMATCH=y
+}
+
+function zupload()
+{
+git clone --depth=1 https://github.com/sarthakroy2002/AnyKernel3.git AnyKernel
+cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+cd AnyKernel
+nano anykernel.sh
+zip -r9 Test-OSS-KERNEL-RMX2001-NEOLIT.zip *
+#curl --upload-file Test-OSS-KERNEL-RMX2020-NEOLIT.zip https://transfer.sh/
+curl -sL https://git.io/file-transfer | sh
+./transfer wet Test-OSS-KERNEL-RMX2001-NEOLIT.zip
+}
+
+compile
+zupload