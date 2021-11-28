JamiKettunen/android_kernel_volla_mt6763

tools: dct: Move to modern Python 3
Now we can drop the Python 2 build-time dependency of this kernel tree!
\o/

tools/dct/DrvGen.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python2
+#! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2016 MediaTek Inc.
@@ -42,7 +42,7 @@ from utility.util import LogLevel
 from utility.util import log
 
 def usage():
-    print '''
+    print('''
 usage: DrvGen [dws_path] [file_path] [log_path] [paras]...
 
 options and arguments:
@@ -51,7 +51,7 @@ dws_path    :    dws file path
 file_path   :    where you want to put generated files
 log_path    :    where to store the log files
 paras        :    parameter for generate wanted file
-'''
+''')
 
 def is_oldDws(path, gen_spec):
     if not os.path.exists(path):
@@ -60,7 +60,7 @@ def is_oldDws(path, gen_spec):
 
     try:
         root = xml.dom.minidom.parse(dws_path)
-    except Exception, e:
+    except Exception:
         log(LogLevel.warn, '%s is not xml format, try to use old DCT!' %(dws_path))
         if len(gen_spec) == 0:
             log(LogLevel.warn, 'Please use old DCT UI to gen all files!')
@@ -139,32 +139,32 @@ if __name__ == '__main__':
     chipId = ChipObj.get_chipId(dws_path)
     log(LogLevel.info, 'chip id: %s' %(chipId))
     chipObj = None
-    if cmp(chipId, 'MT6797') == 0:
+    if chipId == 'MT6797':
         chipObj = Everest(dws_path, gen_path)
-    elif cmp(chipId, 'MT6757') == 0:
+    elif chipId == 'MT6757':
         chipObj = Olympus(dws_path, gen_path)
-    elif cmp(chipId, 'MT6757-P25') == 0:
+    elif chipId == 'MT6757-P25':
         chipObj = MT6757_P25(dws_path, gen_path)
-    elif cmp(chipId, 'KIBOPLUS') == 0:
+    elif chipId == 'KIBOPLUS':
         chipObj = MT6757_P25(dws_path, gen_path)
-    elif cmp(chipId, 'MT6570') == 0:
+    elif chipId == 'MT6570':
         chipObj = Rushmore(dws_path, gen_path)
-    elif cmp(chipId, 'MT6799') == 0:
+    elif chipId == 'MT6799':
         chipObj = Whitney(dws_path, gen_path)
-    elif cmp(chipId, 'MT6763') == 0:
+    elif chipId == 'MT6763':
         chipObj = MT6763(dws_path, gen_path)
-    elif cmp(chipId, 'MT6759') == 0:
+    elif chipId == 'MT6759':
         chipObj = MT6759(dws_path, gen_path)
-    elif cmp(chipId, 'MT6750S') == 0:
+    elif chipId == 'MT6750S':
         chipObj = MT6750S(dws_path, gen_path)
-    elif cmp(chipId, 'MT6758') == 0:
+    elif chipId == 'MT6758':
         chipObj = MT6758(dws_path, gen_path)
-    elif cmp(chipId, 'MT6739') == 0:
+    elif chipId == 'MT6739':
         chipObj = MT6739(dws_path, gen_path)
-    elif cmp(chipId, 'MT8695') == 0:
+    elif chipId == 'MT8695':
         chipObj = MT8695(dws_path, gen_path)
-    elif cmp(chipId, 'MT6771') == 0 or \
-         cmp(chipId, 'MT6775') == 0:
+    elif chipId == 'MT6771' or \
+         chipId == 'MT6775':
         chipObj = MT6771(dws_path, gen_path)
     #elif cmp(chipId, 'MT6775') == 0:
         #chipObj = MT6775(dws_path, gen_path)

sarthakroy2002/kernel_realme_wasabi

Change-Id: I57779dae41e0ee1075db96725e7e6fdcb5704021
Signed-off-by: SamarV-121 <samarvispute121@pm.me>

tools/dct/DrvGen.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python2
+#! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2016 MediaTek Inc.
@@ -47,7 +47,7 @@ from utility.util import LogLevel
 from utility.util import log
 
 def usage():
-    print '''
+    print('''
 usage: DrvGen [dws_path] [file_path] [log_path] [paras]...
 
 options and arguments:
@@ -56,7 +56,7 @@ dws_path    :    dws file path
 file_path   :    where you want to put generated files
 log_path    :    where to store the log files
 paras        :    parameter for generate wanted file
-'''
+''')
 
 def is_oldDws(path, gen_spec):
     if not os.path.exists(path):
@@ -65,7 +65,7 @@ def is_oldDws(path, gen_spec):
 
     try:
         root = xml.dom.minidom.parse(dws_path)
-    except Exception, e:
+    except Exception:
         log(LogLevel.warn, '%s is not xml format, try to use old DCT!' %(dws_path))
         if len(gen_spec) == 0:
             log(LogLevel.warn, 'Please use old DCT UI to gen all files!')
@@ -144,46 +144,46 @@ if __name__ == '__main__':
     chipId = ChipObj.get_chipId(dws_path)
     log(LogLevel.info, 'chip id: %s' %(chipId))
     chipObj = None
-    if cmp(chipId, 'MT6797') == 0:
+    if chipId == 'MT6797':
         chipObj = MT6797(dws_path, gen_path)
-    elif cmp(chipId, 'MT6757') == 0:
+    elif chipId == 'MT6757':
         chipObj = MT6757(dws_path, gen_path)
-    elif cmp(chipId, 'MT6757-P25') == 0:
+    elif chipId == 'MT6757-P25':
         chipObj = MT6757_P25(dws_path, gen_path)
-    elif cmp(chipId, 'MT6570') == 0:
+    elif chipId == 'MT6570':
         chipObj = MT6570(dws_path, gen_path)
-    elif cmp(chipId, 'MT6799') == 0:
+    elif chipId == 'MT6799':
         chipObj = MT6799(dws_path, gen_path)
-    elif cmp(chipId, 'MT6763') == 0:
+    elif chipId == 'MT6763':
         chipObj = MT6763(dws_path, gen_path)
-    elif cmp(chipId, 'MT6759') == 0:
+    elif chipId == 'MT6759':
         chipObj = MT6759(dws_path, gen_path)
-    elif cmp(chipId, 'MT6750S') == 0:
+    elif chipId == 'MT6750S':
         chipObj = MT6750S(dws_path, gen_path)
-    elif cmp(chipId, 'MT6758') == 0:
+    elif chipId == 'MT6758':
         chipObj = MT6758(dws_path, gen_path)
-    elif cmp(chipId, 'MT6739') == 0:
+    elif chipId == 'MT6739':
         chipObj = MT6739(dws_path, gen_path)
-    elif cmp(chipId, 'MT8695') == 0 or \
-         cmp(chipId, 'MT8168') == 0:
+    elif chipId == 'MT8695' or \
+         chipId == 'MT8168':
         chipObj = MT8695(dws_path, gen_path)
-    elif cmp(chipId, 'MT6771') == 0 or \
-         cmp(chipId, 'MT6775') == 0 or \
-         cmp(chipId, 'MT6765') == 0 or \
-         cmp(chipId, 'MT3967') == 0 or \
-         cmp(chipId, 'MT6761') == 0:
+    elif chipId == 'MT6771' or \
+         chipId == 'MT6775' or \
+         chipId == 'MT6765' or \
+         chipId == 'MT3967' or \
+         chipId == 'MT6761':
         chipObj = MT6771(dws_path, gen_path)
-    elif cmp(chipId, 'MT6779') == 0:
+    elif chipId == 'MT6779':
         chipObj = MT6779(dws_path, gen_path)
-    elif cmp(chipId, 'MT6768') == 0:
+    elif chipId == 'MT6768':
         chipObj = MT6768(dws_path, gen_path)
-    elif cmp(chipId, 'MT6785') == 0:
+    elif chipId == 'MT6785':
         chipObj = MT6785(dws_path, gen_path)
-    elif cmp(chipId, 'MT6885') == 0 or \
-         cmp(chipId, 'MT6873') == 0 or \
-         cmp(chipId, 'MT6893') == 0:
+    elif chipId == 'MT6885' or \
+         chipId == 'MT6873' or \
+         chipId == 'MT6893':
         chipObj = MT6885(dws_path, gen_path)
-    elif cmp(chipId, 'MT6853') == 0:
+    elif chipId == 'MT6853':
         chipObj = MT6853(dws_path, gen_path)
     else:
         chipObj = ChipObj(dws_path, gen_path)