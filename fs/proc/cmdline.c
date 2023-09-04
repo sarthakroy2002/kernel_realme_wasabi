c7dff90d998e2704217099b1c08fe5a36bba5c61
Sarthak Roy <sarthakroy2002@gmail.com>
2023-09-04 22:58:49 +0530

    Revert "proc: Remove verifiedbootstate flag from /proc/cmdline"
    This reverts commit 6a43fc183dba57fb297803bbf00bb24e9a30d79b.
    
    Signed-off-by: prathamdubey2005 <134331217+prathamdubey2005@users.noreply.github.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -24,29 +24,6 @@ static const struct file_operations cmdline_proc_fops = {
 
 static int __init proc_cmdline_init(void)
 {
-	char *offset_addr, *cmd = new_command_line;
-
-	strcpy(cmd, saved_command_line);
-
-	/*
-	 * Remove 'androidboot.verifiedbootstate' flag from command line seen
-	 * by userspace in order to pass SafetyNet CTS check.
-	 */
-	offset_addr = strstr(cmd, "androidboot.verifiedbootstate=");
-	if (offset_addr) {
-		size_t i, len, offset;
-
-		len = strlen(cmd);
-		offset = offset_addr - cmd;
-
-		for (i = 1; i < (len - offset); i++) {
-			if (cmd[offset + i] == ' ')
-				break;
-		}
-
-		memmove(offset_addr, &cmd[offset + i + 1], len - i - offset);
-	}
-
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
